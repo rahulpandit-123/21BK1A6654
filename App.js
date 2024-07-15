c9e0fbcb8a395b6e337a9ae779abfb8c9cd6bca5
k rahul pandit <147794873+rahulpandit-123@users.noreply.github.com>
2024-07-15 12:34:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Signup from './e-commerce/Signup';
+import Home from './e-commerce/Home/Home'
+function App() {
+  return (
+    <div>
+     <Signup/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
